876dd7236a1d1f28c98d0d30886f9d66102d7154
Benjamin Leiding <mail@benjaminleiding.com>
2024-11-24 23:21:55 +0100

    + added mindmap paper template * updated presentation content

---
+++ templates/Mind-Map/PaperOutlineTemplate.mm
@@ -0,0 +1,184 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Paper Title" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1732486310257" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.9487174">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="22" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Meta Instructions" POSITION="bottom_or_right" ID="ID_1677629360" CREATED="1655280970061" MODIFIED="1655284219425">
+<edge COLOR="#0000ff"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Github Repository" ID="ID_475476204" CREATED="1655281111710" MODIFIED="1732484293495" LINK="https://github.com/ETCE-LAB/How-To-Write-A-Paper-Thesis/tree/main?tab=readme-ov-file#how-to-write-a-paper-or-a-thesis"/>
+</node>
+<node TEXT="Abstract" POSITION="bottom_or_right" ID="ID_1271960140" CREATED="1655280977805" MODIFIED="1655280980128">
+<edge COLOR="#00ff00"/>
+<node TEXT="Follows Moves 1-4" ID="ID_141806285" CREATED="1655281781898" MODIFIED="1655281817499" LINK="#ID_364780417"/>
+<node TEXT="A Good Abstract:" ID="ID_486293778" CREATED="1732484309375" MODIFIED="1732484348557" COLOR="#000000">
+<node TEXT="1.) What is the paper about?" ID="ID_1389243906" CREATED="1732484354355" MODIFIED="1732484416481"/>
+<node TEXT="2.) What is the State of the Art?" ID="ID_1557756584" CREATED="1732484369137" MODIFIED="1732484421364"/>
+<node TEXT="3.) What is the detected gap?" ID="ID_495214262" CREATED="1732484380905" MODIFIED="1732484425616"/>
+<node TEXT="4.) What are the main questions to be answered pertaining to the gap?" ID="ID_896697753" CREATED="1732484396833" MODIFIED="1732484431961"/>
+<node TEXT="5.) Why is the solution good/better than other solutions?" ID="ID_1650375849" CREATED="1732484404148" MODIFIED="1732484437764"/>
+<node TEXT="Roughly 100-200 words, depending on scope of publication, e.g., seminar paper vs. PhD-Thesis" ID="ID_1787821722" CREATED="1732484445254" MODIFIED="1732484447129"/>
+</node>
+</node>
+<node TEXT="Chapter 1" POSITION="bottom_or_right" ID="ID_364780417" CREATED="1655280982269" MODIFIED="1655284231513">
+<edge COLOR="#ff00ff"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Move 1" ID="ID_1497835010" CREATED="1655281842362" MODIFIED="1655281852939">
+<node TEXT="Set the scene, what the manuscript is about" ID="ID_1857717618" CREATED="1655281852943" MODIFIED="1655281869163"/>
+</node>
+<node TEXT="Move 2" ID="ID_714140715" CREATED="1655281906794" MODIFIED="1655281910507">
+<node TEXT="Existing body of knowledge, state-of-the-art" ID="ID_218131704" CREATED="1655281910510" MODIFIED="1655281929100"/>
+<node TEXT="leads to a gap detection" ID="ID_1812763938" CREATED="1655281930683" MODIFIED="1655282172187" LINK="#ID_1184127498"/>
+</node>
+<node TEXT="Move 3" ID="ID_1322291673" CREATED="1655281955514" MODIFIED="1655281967963">
+<node TEXT="RQ: Research Question" ID="ID_1184127498" CREATED="1655281967967" MODIFIED="1655282012363">
+<node TEXT="RQ1: Research Question" ID="ID_1055482967" CREATED="1655282024970" MODIFIED="1655282033899"/>
+<node TEXT="RQ2: Research Question" ID="ID_434486230" CREATED="1655282042058" MODIFIED="1655282050347"/>
+<node TEXT="RQ3: Research Question" ID="ID_316040170" CREATED="1655282058506" MODIFIED="1655282068555"/>
+</node>
+<node TEXT="Explanation of RQ&gt; RQ1+RQ2+RQ3" ID="ID_544779873" CREATED="1655282079210" MODIFIED="1655282099293"/>
+</node>
+<node TEXT="Move 4" ID="ID_1533314077" CREATED="1655282187979" MODIFIED="1655282193147">
+<node TEXT="Explanation of remaining thesis structure, i.e., Chapters 2-7" ID="ID_44832154" CREATED="1655282193151" MODIFIED="1655282216894"/>
+<node TEXT="Should reveal a research method execution" ID="ID_1497055419" CREATED="1655282217483" MODIFIED="1655282331932">
+<node TEXT="Action Research" ID="ID_1830817359" CREATED="1655282229424" MODIFIED="1732484698184" LINK="https://journals.sagepub.com/doi/pdf/10.1177/14767503030011002"/>
+<node TEXT="Design Science Research (DSR)" ID="ID_312729459" CREATED="1655282234619" MODIFIED="1732484669245" LINK="https://www.jstor.org/stable/25148625"/>
+<node TEXT="Action Design Research" ID="ID_787945297" CREATED="1655282239467" MODIFIED="1732484728458" LINK="https://www.jstor.org/stable/23043488" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="1.5 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="Chapter 2" POSITION="bottom_or_right" ID="ID_73503457" CREATED="1655280990381" MODIFIED="1655284239473">
+<edge COLOR="#00ffff"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Bridge chapter" LOCALIZED_STYLE_REF="styles.important" ID="ID_450593050" CREATED="1655282348411" MODIFIED="1655282441426">
+<node TEXT="to understand Chapters 3-6" ID="ID_803878923" CREATED="1655282456795" MODIFIED="1655282471853"/>
+</node>
+<node TEXT="Typically expands Move 2 (state-of-the-art)" ID="ID_793690552" CREATED="1655282487515" MODIFIED="1655282509899" LINK="#ID_714140715"/>
+</node>
+<node TEXT="Optional appendices" POSITION="top_or_left" ID="ID_62362799" CREATED="1655283941089" MODIFIED="1732486622996">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Chapter 3" POSITION="bottom_or_right" ID="ID_1855518768" CREATED="1655281001805" MODIFIED="1655284235870">
+<edge COLOR="#007c00"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Introduction with motivation" ID="ID_245073645" CREATED="1655282527515" MODIFIED="1655282534093"/>
+<node TEXT="Related Work" ID="ID_1139713739" CREATED="1655282989691" MODIFIED="1655282994828"/>
+<node TEXT="Main part" ID="ID_936814218" CREATED="1655282953067" MODIFIED="1655282956492">
+<node TEXT="Answers RQ1" ID="ID_779847592" CREATED="1655282956495" MODIFIED="1732486423983"/>
+<node TEXT="A1 answer" ID="ID_776327469" CREATED="1655282976795" MODIFIED="1732486604122"/>
+</node>
+</node>
+<node TEXT="Bibliography" POSITION="top_or_left" ID="ID_63321270" CREATED="1655283919344" MODIFIED="1655284212313">
+<edge COLOR="#007c7c"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Core literature" ID="ID_648426967" CREATED="1655283927233" MODIFIED="1655283932803"/>
+<node TEXT="Supporting literature" ID="ID_1844581582" CREATED="1655283933681" MODIFIED="1655283937203"/>
+</node>
+<node TEXT="Chapter 7" POSITION="top_or_left" ID="ID_1005556731" CREATED="1655283786450" MODIFIED="1655284035602">
+<edge COLOR="#7c007c"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Conclusion" LOCALIZED_STYLE_REF="styles.important" ID="ID_41211601" CREATED="1655283810401" MODIFIED="1655283819510">
+<node TEXT="Paper summary" ID="ID_1949787458" CREATED="1655283821505" MODIFIED="1732486572666"/>
+<node TEXT="Answer to RQ generated out of A &gt; A1+A2+A3" ID="ID_1224458689" CREATED="1655283839137" MODIFIED="1655283858403"/>
+<node TEXT="Outlook" ID="ID_156186140" CREATED="1655283858881" MODIFIED="1732486612353"/>
+</node>
+</node>
+<node TEXT="Chapter 6" POSITION="top_or_left" ID="ID_804291191" CREATED="1655283610963" MODIFIED="1655284029347" HGAP_QUANTITY="11.75 pt" VSHIFT_QUANTITY="15.75 pt">
+<edge COLOR="#007c00"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Evaluation" LOCALIZED_STYLE_REF="styles.important" ID="ID_46120102" CREATED="1655283615395" MODIFIED="1655283626839">
+<node TEXT="for findings in Chapters 3-5" ID="ID_940072429" CREATED="1655283668754" MODIFIED="1655283679908"/>
+</node>
+<node TEXT="Case Study, prototyping, etc." ID="ID_1039896261" CREATED="1655283681874" MODIFIED="1655283693636"/>
+<node TEXT="Differs depending on the chosen research method" ID="ID_918171706" CREATED="1655283695026" MODIFIED="1655283738898" LINK="#ID_1497055419"/>
+</node>
+<node TEXT="Chapter 5" POSITION="top_or_left" ID="ID_1379486902" CREATED="1655281001805" MODIFIED="1655283440134">
+<edge COLOR="#007c00"/>
+<node TEXT="Introduction with motivation" ID="ID_738032779" CREATED="1655282527515" MODIFIED="1655282534093"/>
+<node TEXT="Related Work" ID="ID_260108054" CREATED="1655282989691" MODIFIED="1655282994828"/>
+<node TEXT="Main part" ID="ID_1660896287" CREATED="1655282953067" MODIFIED="1655282956492">
+<node TEXT="Answers RQ3" ID="ID_1943112215" CREATED="1655282956495" MODIFIED="1732486519947"/>
+<node TEXT="A3 answer" ID="ID_1079099905" CREATED="1655282966667" MODIFIED="1732486587963"/>
+</node>
+</node>
+<node TEXT="Chapter 4" POSITION="top_or_left" ID="ID_1593234357" CREATED="1655281001805" MODIFIED="1655283224457">
+<edge COLOR="#007c00"/>
+<node TEXT="Introduction with motivation" ID="ID_732740544" CREATED="1655282527515" MODIFIED="1655282534093"/>
+<node TEXT="Related Work" ID="ID_105244185" CREATED="1655282989691" MODIFIED="1655282994828"/>
+<node TEXT="Main part" ID="ID_746021911" CREATED="1655282953067" MODIFIED="1655282956492">
+<node TEXT="Answers RQ2" ID="ID_1239638267" CREATED="1655282956495" MODIFIED="1732486494835"/>
+<node TEXT="A2 answer" ID="ID_1316290173" CREATED="1655282976795" MODIFIED="1732486593522"/>
+</node>
+</node>
+</node>
+</map>
