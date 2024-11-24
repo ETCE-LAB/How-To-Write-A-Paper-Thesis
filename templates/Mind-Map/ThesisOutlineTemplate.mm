e238ed7af7e22cb2002d806fc193616bf0a7b80d
Benjamin Leiding <mail@benjaminleiding.com>
2024-11-24 22:30:49 +0100

    * INIT

---
+++ templates/Mind-Map/ThesisOutlineTemplate.mm
@@ -0,0 +1,264 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Thesis Title" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1655284018276" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.071">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="22" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Meta Instructions" POSITION="right" ID="ID_1677629360" CREATED="1655280970061" MODIFIED="1655284219425">
+<edge COLOR="#0000ff"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Good thinking in general" ID="ID_475476204" CREATED="1655281111710" MODIFIED="1655281126014">
+<node TEXT="Trivium and quadrivium" ID="ID_482719966" CREATED="1655281281213" MODIFIED="1655281683570" LINK="http://www.triviumeducation.com/"/>
+<node TEXT="Lecture on TriviumQuandrivium" ID="ID_1828877520" CREATED="1655281292891" MODIFIED="1655281695344" LINK="http://www.triviumeducation.com/19-2/"/>
+<node TEXT="Thinking as a science" ID="ID_130524826" CREATED="1655281307163" MODIFIED="1655281704768" LINK="https://mises.org/document/3456/"/>
+<node TEXT="Logical fallacies" ID="ID_148367821" CREATED="1655281312507" MODIFIED="1655281319085">
+<node TEXT="Wikipedia" ID="ID_1751641054" CREATED="1655281354955" MODIFIED="1655281715408" LINK="http://en.wikipedia.org/wiki/List_of_fallacies"/>
+<node TEXT="Video Lecture" ID="ID_1473015394" CREATED="1655281363179" MODIFIED="1655281735152" LINK="https://www.youtube.com/watch?v=MhhJAGjBIEw"/>
+</node>
+</node>
+<node TEXT="How to write a scientific paper" ID="ID_1974461326" CREATED="1655281134012" MODIFIED="1655284347125" LINK="http://www.cs.wayne.edu/~shiyong/howtowriteapaper.pdf"/>
+<node TEXT="How NOT to write a paper" ID="ID_1877213885" CREATED="1655284351696" MODIFIED="1655284370629" LINK="https://people.csail.mit.edu/alinush/communication/writing.pdf"/>
+<node TEXT="Mindmaps" ID="ID_114755740" CREATED="1655281142540" MODIFIED="1655281261975" LINK="https://en.wikipedia.org/wiki/Mind_map"/>
+</node>
+<node TEXT="Abstract" POSITION="right" ID="ID_1271960140" CREATED="1655280977805" MODIFIED="1655280980128">
+<edge COLOR="#00ff00"/>
+<node TEXT="Follows Moves 1-4" ID="ID_141806285" CREATED="1655281781898" MODIFIED="1655281817499" LINK="#ID_364780417"/>
+</node>
+<node TEXT="Chapter 1" POSITION="right" ID="ID_364780417" CREATED="1655280982269" MODIFIED="1655284231513">
+<edge COLOR="#ff00ff"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Move 1" ID="ID_1497835010" CREATED="1655281842362" MODIFIED="1655281852939">
+<node TEXT="Set the scene, what the manuscript is about" ID="ID_1857717618" CREATED="1655281852943" MODIFIED="1655281869163">
+<node TEXT="Formal Logic" ID="ID_791728102" CREATED="1655281877626" MODIFIED="1655281894951" LINK="http://www.triviumeducation.com/logic/"/>
+</node>
+</node>
+<node TEXT="Move 2" ID="ID_714140715" CREATED="1655281906794" MODIFIED="1655281910507">
+<node TEXT="Existing body of knowledge, state-of-the-art" ID="ID_218131704" CREATED="1655281910510" MODIFIED="1655281929100"/>
+<node TEXT="leads to a gap detection" ID="ID_1812763938" CREATED="1655281930683" MODIFIED="1655282172187" LINK="#ID_1184127498"/>
+</node>
+<node TEXT="Move 3" ID="ID_1322291673" CREATED="1655281955514" MODIFIED="1655281967963">
+<node TEXT="RQ: Research Question" ID="ID_1184127498" CREATED="1655281967967" MODIFIED="1655282012363">
+<node TEXT="Classical rhetoric" ID="ID_1915797844" CREATED="1655282012368" MODIFIED="1655282118726" LINK="http://www.triviumeducation.com/rhetoric/"/>
+<node TEXT="RQ1: Research Question" ID="ID_1055482967" CREATED="1655282024970" MODIFIED="1655282033899">
+<node TEXT="Classical rhetoric" ID="ID_131909336" CREATED="1655282033903" MODIFIED="1655282121937" LINK="http://www.triviumeducation.com/rhetoric/"/>
+</node>
+<node TEXT="RQ2: Research Question" ID="ID_434486230" CREATED="1655282042058" MODIFIED="1655282050347">
+<node TEXT="Classical rhetoric" ID="ID_1077343661" CREATED="1655282050351" MODIFIED="1655282125329" LINK="http://www.triviumeducation.com/rhetoric/"/>
+</node>
+<node TEXT="RQ3: Research Question" ID="ID_316040170" CREATED="1655282058506" MODIFIED="1655282068555">
+<node TEXT="Classical rhetoric" ID="ID_530497377" CREATED="1655282068558" MODIFIED="1655282155312" LINK="http://www.triviumeducation.com/rhetoric/"/>
+</node>
+</node>
+<node TEXT="Explanation of RQ&gt; RQ1+RQ2+RQ3" ID="ID_544779873" CREATED="1655282079210" MODIFIED="1655282099293"/>
+</node>
+<node TEXT="Move 4" ID="ID_1533314077" CREATED="1655282187979" MODIFIED="1655282193147">
+<node TEXT="Explanation of remaining thesis structure, i.e., Chapters 2-7" ID="ID_44832154" CREATED="1655282193151" MODIFIED="1655282216894"/>
+<node TEXT="Should reveal a research method execution" ID="ID_1497055419" CREATED="1655282217483" MODIFIED="1655282331932">
+<node TEXT="Action science" ID="ID_1830817359" CREATED="1655282229424" MODIFIED="1655282256128" LINK="https://en.wikipedia.org/wiki/Action_research"/>
+<node TEXT="Design science" ID="ID_312729459" CREATED="1655282234619" MODIFIED="1655282269760" LINK="http://www2.uiah.fi/~ikoskine/ip07/r0201a_Hevner.pdf"/>
+<node TEXT="Action-Design science" ID="ID_787945297" CREATED="1655282239467" MODIFIED="1655282331931" LINK="http://bada.hb.se/bitstream/2320/9888/1/Sein%20et%20al%20%282011%29.pdf" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="1.5 pt"/>
+</node>
+<node TEXT="Research method follows quadrivium" ID="ID_1833315870" CREATED="1655282296475" MODIFIED="1655282320145" LINK="http://bada.hb.se/bitstream/2320/9888/1/Sein%20et%20al%20%282011%29.pdf"/>
+</node>
+</node>
+<node TEXT="Chapter 2" POSITION="right" ID="ID_73503457" CREATED="1655280990381" MODIFIED="1655284239473">
+<edge COLOR="#00ffff"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Bridge chapter" LOCALIZED_STYLE_REF="styles.important" ID="ID_450593050" CREATED="1655282348411" MODIFIED="1655282441426">
+<node TEXT="to understand Chapters 3-6" ID="ID_803878923" CREATED="1655282456795" MODIFIED="1655282471853"/>
+</node>
+<node TEXT="Typically expands Move 2 (state-of-the-art)" ID="ID_793690552" CREATED="1655282487515" MODIFIED="1655282509899" LINK="#ID_714140715"/>
+</node>
+<node TEXT="Appendices" POSITION="left" ID="ID_62362799" CREATED="1655283941089" MODIFIED="1655283945155">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Chapter 3" POSITION="right" ID="ID_1855518768" CREATED="1655281001805" MODIFIED="1655284235870">
+<edge COLOR="#007c00"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Introduction with motivation" ID="ID_245073645" CREATED="1655282527515" MODIFIED="1655282534093">
+<node TEXT="Formal Logic" ID="ID_565506469" CREATED="1655282534411" MODIFIED="1655282539037"/>
+<node TEXT="RQ1: Research Question" ID="ID_14375000" CREATED="1655282544603" MODIFIED="1655282903676" LINK="#ID_1055482967">
+<node TEXT="Classical rhetoric" ID="ID_1414146193" CREATED="1655282012368" MODIFIED="1655282118726" LINK="http://www.triviumeducation.com/rhetoric/"/>
+<node TEXT="RQ1.1 Sub-research question" ID="ID_690971728" CREATED="1655282566747" MODIFIED="1655282578604">
+<node TEXT="general grammar" ID="ID_1983850791" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+<node TEXT="RQ1.2 Sub-research question" ID="ID_1162981881" CREATED="1655282584731" MODIFIED="1655282766687">
+<node TEXT="general grammar" ID="ID_1467407952" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+<node TEXT="RQ1.3 Sub-research question" ID="ID_611547627" CREATED="1655282599371" MODIFIED="1655282608669">
+<node TEXT="general grammar" ID="ID_303093817" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+</node>
+</node>
+<node TEXT="Main part" ID="ID_936814218" CREATED="1655282953067" MODIFIED="1655282956492">
+<node TEXT="Section 3.1: Answers RQ1.1" ID="ID_779847592" CREATED="1655282956495" MODIFIED="1655282966061"/>
+<node TEXT="Section 3.2: Answers RQ1.2" ID="ID_667611363" CREATED="1655282966667" MODIFIED="1655282976445"/>
+<node TEXT="Section 3.3: Answers RQ1.3" ID="ID_776327469" CREATED="1655282976795" MODIFIED="1655282988013"/>
+</node>
+<node TEXT="Related Work" ID="ID_1139713739" CREATED="1655282989691" MODIFIED="1655282994828"/>
+<node TEXT="Conclusion" ID="ID_1397595616" CREATED="1655282995146" MODIFIED="1655282998363">
+<node TEXT="S1: Summary" ID="ID_1934768441" CREATED="1655282998366" MODIFIED="1655283003308"/>
+<node TEXT="C1: Conclusion" ID="ID_834416562" CREATED="1655283003930" MODIFIED="1655283010811">
+<node TEXT="Answers RQ1, RQ1.1, RQ1.2, RQ1.3" LOCALIZED_STYLE_REF="styles.important" ID="ID_562571298" CREATED="1655283010814" MODIFIED="1655283135911"/>
+<node TEXT="A1 &gt; A1.1 + A1.2 + A1.3" ID="ID_704888374" CREATED="1655283026346" MODIFIED="1655283042268"/>
+</node>
+<node TEXT="O1: Outlook for future work (related-work inspired)" ID="ID_1699669825" CREATED="1655283077785" MODIFIED="1655283102267"/>
+</node>
+</node>
+<node TEXT="Bibliography" POSITION="left" ID="ID_63321270" CREATED="1655283919344" MODIFIED="1655284212313">
+<edge COLOR="#007c7c"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Core literature" ID="ID_648426967" CREATED="1655283927233" MODIFIED="1655283932803"/>
+<node TEXT="Supporting literature" ID="ID_1844581582" CREATED="1655283933681" MODIFIED="1655283937203"/>
+</node>
+<node TEXT="Chapter 7" POSITION="left" ID="ID_1005556731" CREATED="1655283786450" MODIFIED="1655284035602">
+<edge COLOR="#7c007c"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Introduction with motivation" ID="ID_51959763" CREATED="1655283795490" MODIFIED="1655283800851"/>
+<node TEXT="Discussion of related work" ID="ID_999370447" CREATED="1655283801874" MODIFIED="1655283809540"/>
+<node TEXT="Conclusion" LOCALIZED_STYLE_REF="styles.important" ID="ID_41211601" CREATED="1655283810401" MODIFIED="1655283819510">
+<node TEXT="Thesis summary S &gt; S1+S2+S3" ID="ID_1949787458" CREATED="1655283821505" MODIFIED="1655283838451"/>
+<node TEXT="Answer to RQ generated out of A &gt; A1+A2+A3" ID="ID_1224458689" CREATED="1655283839137" MODIFIED="1655283858403"/>
+<node TEXT="Outlook O &gt; O1+O2+O3" ID="ID_156186140" CREATED="1655283858881" MODIFIED="1655283875315"/>
+</node>
+</node>
+<node TEXT="Chapter 6" POSITION="left" ID="ID_804291191" CREATED="1655283610963" MODIFIED="1655284029347" HGAP_QUANTITY="11.75 pt" VSHIFT_QUANTITY="15.75 pt">
+<edge COLOR="#007c00"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Evaluation" LOCALIZED_STYLE_REF="styles.important" ID="ID_46120102" CREATED="1655283615395" MODIFIED="1655283626839">
+<node TEXT="for findings in Chapters 3-5" ID="ID_940072429" CREATED="1655283668754" MODIFIED="1655283679908"/>
+</node>
+<node TEXT="Case Study, prototyping, etc." ID="ID_1039896261" CREATED="1655283681874" MODIFIED="1655283693636"/>
+<node TEXT="Differs depending on the chosen research method" ID="ID_918171706" CREATED="1655283695026" MODIFIED="1655283738898" LINK="#ID_1497055419"/>
+</node>
+<node TEXT="Chapter 5" POSITION="left" ID="ID_1379486902" CREATED="1655281001805" MODIFIED="1655283440134">
+<edge COLOR="#007c00"/>
+<node TEXT="Introduction with motivation" ID="ID_738032779" CREATED="1655282527515" MODIFIED="1655282534093">
+<node TEXT="Formal Logic" ID="ID_1129091453" CREATED="1655282534411" MODIFIED="1655283396638" LINK="http://www.triviumeducation.com/logic/"/>
+<node TEXT="RQ3: Research Question" ID="ID_496233629" CREATED="1655282544603" MODIFIED="1655283564275" LINK="#ID_316040170">
+<node TEXT="Classical rhetoric" ID="ID_1818850330" CREATED="1655282012368" MODIFIED="1655282118726" LINK="http://www.triviumeducation.com/rhetoric/"/>
+<node TEXT="RQ3.1 Sub-research question" ID="ID_843851345" CREATED="1655282566747" MODIFIED="1655283453847">
+<node TEXT="general grammar" ID="ID_784602213" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+<node TEXT="RQ3.2 Sub-research question" ID="ID_1407952234" CREATED="1655282584731" MODIFIED="1655283456246">
+<node TEXT="general grammar" ID="ID_950715549" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+<node TEXT="RQ3.3 Sub-research question" ID="ID_1291591069" CREATED="1655282599371" MODIFIED="1655283458679">
+<node TEXT="general grammar" ID="ID_1749884014" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+</node>
+</node>
+<node TEXT="Main part" ID="ID_1660896287" CREATED="1655282953067" MODIFIED="1655282956492">
+<node TEXT="Section 5.1: Answers RQ3.1" ID="ID_1943112215" CREATED="1655282956495" MODIFIED="1655283483223"/>
+<node TEXT="Section 5.2: Answers RQ3.2" ID="ID_1079099905" CREATED="1655282966667" MODIFIED="1655283486454"/>
+<node TEXT="Section 5.3: Answers RQ3.3" ID="ID_695582549" CREATED="1655282976795" MODIFIED="1655283489062"/>
+</node>
+<node TEXT="Related Work" ID="ID_260108054" CREATED="1655282989691" MODIFIED="1655282994828"/>
+<node TEXT="Conclusion" ID="ID_42175418" CREATED="1655282995146" MODIFIED="1655282998363">
+<node TEXT="S3: Summary" ID="ID_1205525012" CREATED="1655282998366" MODIFIED="1655283492790"/>
+<node TEXT="C3: Conclusion" ID="ID_878002403" CREATED="1655283003930" MODIFIED="1655283495494">
+<node TEXT="Answers RQ3, RQ3.1, RQ3.2, RQ3.3" LOCALIZED_STYLE_REF="styles.important" ID="ID_857487257" CREATED="1655283010814" MODIFIED="1655283509669"/>
+<node TEXT="A3 &gt; A3.1 + A3.2 + A3.3" ID="ID_352322861" CREATED="1655283026346" MODIFIED="1655283522150"/>
+</node>
+<node TEXT="O3: Outlook for future work (related-work inspired)" ID="ID_512095411" CREATED="1655283077785" MODIFIED="1655283526486"/>
+</node>
+</node>
+<node TEXT="Chapter 4" POSITION="left" ID="ID_1593234357" CREATED="1655281001805" MODIFIED="1655283224457">
+<edge COLOR="#007c00"/>
+<node TEXT="Introduction with motivation" ID="ID_732740544" CREATED="1655282527515" MODIFIED="1655282534093">
+<node TEXT="Formal Logic" ID="ID_1979488116" CREATED="1655282534411" MODIFIED="1655283396638" LINK="http://www.triviumeducation.com/logic/"/>
+<node TEXT="RQ2: Research Question" ID="ID_1504231553" CREATED="1655282544603" MODIFIED="1655283574755" LINK="#ID_434486230">
+<node TEXT="Classical rhetoric" ID="ID_366014587" CREATED="1655282012368" MODIFIED="1655282118726" LINK="http://www.triviumeducation.com/rhetoric/"/>
+<node TEXT="RQ2.1 Sub-research question" ID="ID_749156659" CREATED="1655282566747" MODIFIED="1655283239497">
+<node TEXT="general grammar" ID="ID_1835256012" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+<node TEXT="RQ2.2 Sub-research question" ID="ID_568415367" CREATED="1655282584731" MODIFIED="1655283250297">
+<node TEXT="general grammar" ID="ID_1204194896" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+<node TEXT="RQ2.3 Sub-research question" ID="ID_752984653" CREATED="1655282599371" MODIFIED="1655283254184">
+<node TEXT="general grammar" ID="ID_921992413" CREATED="1655282784363" MODIFIED="1655282792514" LINK="http://www.triviumeducation.com/grammar/"/>
+</node>
+</node>
+</node>
+<node TEXT="Main part" ID="ID_746021911" CREATED="1655282953067" MODIFIED="1655282956492">
+<node TEXT="Section 4.1: Answers RQ2.1" ID="ID_1239638267" CREATED="1655282956495" MODIFIED="1655283318329"/>
+<node TEXT="Section 4.2: Answers RQ2.2" ID="ID_1429682843" CREATED="1655282966667" MODIFIED="1655283324678"/>
+<node TEXT="Section 4.3: Answers RQ2.3" ID="ID_1316290173" CREATED="1655282976795" MODIFIED="1655283328680"/>
+</node>
+<node TEXT="Related Work" ID="ID_105244185" CREATED="1655282989691" MODIFIED="1655282994828"/>
+<node TEXT="Conclusion" ID="ID_1289893048" CREATED="1655282995146" MODIFIED="1655282998363">
+<node TEXT="S2: Summary" ID="ID_947071545" CREATED="1655282998366" MODIFIED="1655283332120"/>
+<node TEXT="C2: Conclusion" ID="ID_1693260229" CREATED="1655283003930" MODIFIED="1655283334648">
+<node TEXT="Answers RQ2, RQ2.1, RQ2.2, RQ2.3" LOCALIZED_STYLE_REF="styles.important" ID="ID_843455747" CREATED="1655283010814" MODIFIED="1655283347671"/>
+<node TEXT="A2 &gt; A2.1 + A2.2 + A2.3" ID="ID_1849982654" CREATED="1655283026346" MODIFIED="1655283374903"/>
+</node>
+<node TEXT="O2: Outlook for future work (related-work inspired)" ID="ID_829228178" CREATED="1655283077785" MODIFIED="1655283379863"/>
+</node>
+</node>
+</node>
+</map>
